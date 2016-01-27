e681b804e523c0ae8a1ae5f298c78ea5f5a4f193
George Holevas <gholevas1@gmail.com>
2016-01-27 12:08:17 -0500

    finished page specs

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,36 +1,36 @@
-var expect = require('chai').expect;
-var chai = require('chai');
+// var expect = require('chai').expect;
+// var chai = require('chai');
 
-var spies = require('chai-spies');
-chai.use(spies);
-describe('BS tests while learning mocha', function() {
-    var posNum, negNum;
-    beforeEach(function() {
+// var spies = require('chai-spies');
+// chai.use(spies);
+// describe('BS tests while learning mocha', function() {
+//     var posNum, negNum;
+//     beforeEach(function() {
 
-    });
-    it('sums 2+2', function() {
-        expect(2 + 2).to.equal(4);
-    })
+//     });
+//     it('sums 2+2', function() {
+//         expect(2 + 2).to.equal(4);
+//     })
 
-    it('1000ms timeout is about 1 second', function(done) {
-        var start = new Date();
-        setTimeout(function() {
-            var duration = new Date() - start;
-            expect(duration).to.be.closeTo(1000, 50);
-            done();
-        }, 1000);
-    });
-    it('expect forEach to call on each element', function() {
-        var arr = [1, 2, 3, 4]
+//     it('1000ms timeout is about 1 second', function(done) {
+//         var start = new Date();
+//         setTimeout(function() {
+//             var duration = new Date() - start;
+//             expect(duration).to.be.closeTo(1000, 50);
+//             done();
+//         }, 1000);
+//     });
+//     it('expect forEach to call on each element', function() {
+//         var arr = [1, 2, 3, 4]
 
 
-            var eachFunc = function(index) {
-                console.log(index);
-            }
+//             var eachFunc = function(index) {
+//                 console.log(index);
+//             }
    
-        var each = chai.spy(eachFunc);
+//         var each = chai.spy(eachFunc);
 
-        arr.forEach(each)
-        expect(each).to.have.been.called(4);
-    })
-});
+//         arr.forEach(each)
+//         expect(each).to.have.been.called(4);
+//     })
+// });
--- test/page.spec.js
+++ test/page.spec.js
@@ -1,36 +1,169 @@
-// describe('Route Tests', function () {
-// 	describe('Wiki Routes', function () {});
-//     describe('User Routes', function () {});
+var mocha = require('mocha');
+var expect = require('chai').expect;
+var chai = require('chai');
+var spies = require('chai-spies');
+chai.use(spies);
+var Page = require('../models').Page;
 
 
 
-// });
-// describe('Model Tests', function () {
-describe('Page Tests', function () {
-	describe('Validations') {
-		xit('Errors without required fields (title and content)') {};
-	};
-	describe('Methods') {
-		xit('findSimilar never gets itself') {};
-		xit('findSimilar gets pages with similar tags') {};
-		xit('findSimilar does not get pages without common tags') {};		
-	};
-	describe('Statics') {
-		xit('findByTag gets pages with search tag') {};
-		xit('findByTag does not get pages without search tag') {};
-	};
-	describe('Virtuals') {
-		xit('Returns url name prepended by "/wiki/"'){};		
-	};
-	describe('Hooks') {
-		xit('Sets urlTitle based on title'){};
-	};
+describe('Page model', function() {
+    describe('Validations', function() {
+        var newPage;
+        beforeEach(function() {
+            newPage = new Page();
+        });
+
+        it('errors without title', function(done) {
+            newPage.save()
+                .then(null, function(err) {
+                    expect(err.errors).to.have.property('title');
+                    done();
+                })
+        });
+        it('errors without content', function(done) {
+            newPage.save()
+                .then(null, function(err) {
+                    expect(err.errors).to.have.property('content');
+                    done();
+                })
+        });
+    });
+
+    describe('Statics', function() {
+        beforeEach(function(done) {
+            Page.create({
+                    title: 'I am a title',
+                    content: 'blah',
+                    tags: ['hi', 'unicorn']
+                }, done) //create takes an object and a callback that accepts an error and another input; 
+                //by using done, we are only looking at the error (even though multiple objects are passed)
+        });
+        afterEach(function(done) {
+            Page.remove({}, done);
+        })
+        describe('findByTag', function() {
+            it('gets pages with the search tag', function(done) {
+                Page.findByTag('unicorn') //this returns an array of documents
+                    .then(function(page) {
+                        expect(page.length).to.equal(1);
+                        done();
+                    }).then(null, done);
+            });
+            it('does not get pages without the search tag', function(done) {
+                Page.findByTag() //this returns an array of documents
+                    .then(function(page) {
+                        expect(page.length).to.equal(0);
+                        done();
+                    }).then(null, done);
+            });
+        });
+    });
+
+    describe('Methods', function() {
+        beforeEach(function(done) {
+            Page.create({
+                    title: 'title',
+                    content: 'blah',
+                    tags: ['hi', 'bike']
+                })
+                .then(function() {
+                    Page.create({
+                        title: '2title',
+                        content: 'blah',
+                        tags: ['hi', 'unicorn']
+                    })
+                })
+                .then(function() {
+                    Page.create({
+                        title: '3title',
+                        content: 'blah',
+                        tags: ['dgxfchvj', 'dxgfchgjvh']
+                    })
+                })
+                .then(function() {
+                    done();
+                })
+        });
+
+        afterEach(function(done) {
+            Page.remove({}, done);
+        })
+        describe('findSimilar', function() {
+            it('never gets itself', function(done) {
+                Page.find({
+                        title: 'title'
+                    })
+                    .then(function(pages) {
+                        return pages[0].findSimilar();
+                    })
+                    .then(function(stuff) {
+                        expect(stuff[0].title).to.not.equal('title');
+                        done();
+                    })
+            });
+            it('gets other pages with any common tags', function() {
+                Page.find({
+                        title: 'title'
+                    })
+                    .then(function(pages) {
+                        return pages[0].findSimilar();
+                    })
+                    .then(function(stuff) {
+                        expect(stuff[0].title).to.equal('2title');
+                        done();
+                    })
+            });
+            it('does not get other pages without any common tags', function() {
+                Page.find({
+                        title: 'title'
+                    })
+                    .then(function(pages) {
+                        return pages[0].findSimilar();
+                    })
+                    .then(function(stuff) {
+                        expect(stuff[0].title).to.not.equal('3title');
+                        done();
+                    })
+            });
+        });
+    });
+
+    describe('Virtuals', function() {
+        describe('route', function() {
+            var newPage;
+            beforeEach(function() {
+            	newPage = new Page({
+            		title: 'title space',
+                    urlTitle: 'title_space',
+                    content: 'blah'
+            	}); //with new page, this doesn't save to the db, so it doesn't go by validations yet
+            });
+            it('returns the url_name prepended by "/wiki/"', function(done) {
+            	expect(newPage.route).to.equal('/wiki/title_space');
+            	done();
+            });
+        });
+    });
+
+    describe('Hooks', function() {
+    	          var newPage;
+            beforeEach(function(done) {
+            	Page.create({
+            		title: 'title space',
+                    content: 'blah'
+            	})
+            	.then(function(page) {
+            		newPage = page;
+            	})
+            	.then(function() {
+            		done();
+            	})
+            });
+        it('it sets urlTitle based on title before validating', function(done) {
+        	expect(newPage.urlTitle).to.equal('title_space');
+        	done()
+        });
+    });
+
 });
-describe('User Tests', function () {});
-
-// });
-
-    // describe('A subcategory', function () {
-    //     xit('tests something', function () {});
-    //     xit('tests another aspect of the same thing' , function () {});
-    // });
